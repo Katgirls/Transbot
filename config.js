bc37acec69041f796cd4048b60cc072ac4d496bb
kate <kate@fennet.rentals>
2023-04-18 21:58:03 -0400

    Get all user-defined variables from config.js
    Still get email / password / token from env
    
    Signed-off-by: kate <kate@fennet.rentals>

--- config.js
+++ config.js
@@ -1,9 +1,30 @@
+// bot settings
+const ms_email = process.env.EMAIL;
+const ms_password = process.env.PASSWORD;
+const bot_token = process.env.TOKEN;
+const channel_id = `***REMOVED***`;
+
+// game settings
+const mc_server = "play.fennet.rentals";
+const mc_version = "1.19.3";
+
+// in-game settings
 const whitelist = ["***REMOVED***", "cat_yawn", "Egirl39", "***REMOVED***"];
 const RANGE_GOAL = 1;
 
+// not constants
+let do_look = true;
+let do_farm = false;
+
 module.exports = {
+  ms_email,
+  ms_password,
+  bot_token,
+  channel_id,
+  mc_server,
+  mc_version,
   whitelist,
   RANGE_GOAL,
-  do_look: true,
-  do_farm: false,
+  do_look,
+  do_farm,
 };
