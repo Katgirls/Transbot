44353a050d70d1d1c366369a3b1fd004cce7951a
kate <kate@fennet.rentals>
2023-04-20 09:18:01 -0400

    make MC server port configurable
    Signed-off-by: kate <kate@fennet.rentals>

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 // bot settings
-const ms_email = "***REMOVED***";
+const ms_email    = "***REMOVED***";
 const ms_password = "***REMOVED***";
-const bot_token =
-  "***REMOVED***";
-const channel_id = `***REMOVED***`;
+const bot_token   = "***REMOVED***";
+const channel_id  = "***REMOVED***";
 
 // game settings
-const mc_server = "play.fennet.rentals";
-const mc_version = "1.19.3";
+const mc_server   = "play.fennet.rentals";
+const mc_port     = "25565";
+const mc_version  = "1.19.3";
 
 // in-game settings
-const whitelist = ["***REMOVED***", "cat_yawn", "Egirl39", "***REMOVED***"];
-const RANGE_GOAL = 1;
+const whitelist   = ["***REMOVED***", "cat_yawn", "Egirl39", "***REMOVED***"];
+const RANGE_GOAL  = 1;
 
 // not constants
 let do_look = true;
@@ -23,6 +23,7 @@ module.exports = {
   bot_token,
   channel_id,
   mc_server,
+  mc_port,
   mc_version,
   whitelist,
   RANGE_GOAL,
