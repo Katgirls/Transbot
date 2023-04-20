265f95d14ff13aacc89b5b60fe657fc9afdb970e
FelixCharm <105816617+FelixCharm@users.noreply.github.com>
2023-04-20 15:17:16 +0100

    Created features.js + Fixed commands

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const mineflayer = require("mineflayer");
-const https = require("https");
 const Discord = require("discord.js");
 const uuid = require("uuid-1345");
 
@@ -13,6 +12,7 @@ const farm = require("./farm.js");
 const chat = require("./chat.js");
 const commands = require("./commands.js");
 const config = require("./config.js");
+const features = require("./features.js");
 
 function setup() {
   const { MessageContent, GuildMessages, Guilds } = GatewayIntentBits;
@@ -50,6 +50,7 @@ function setup() {
     farm.set_bot(bot);
     commands.set_bot(bot);
     chat.set_bot(bot);
+    features.set_bot(bot);
 
     advertise();
 
@@ -192,126 +193,10 @@ function setup() {
     channel.send({ embeds: [messageEmbed] });
   });
 
-  function dropAll() {
-    const excludedItems = ["bread"];
-    const item = bot.inventory
-      .items()
-      .find((item) => !excludedItems.includes(item.name));
-    if (item) {
-      bot
-        .tossStack(item)
-        .then(() => {
-          setTimeout(dropAll);
-        })
-        .catch((err) => {
-          console.log(err);
-          setTimeout(dropAll, 100);
-        });
-    }
-  }
-
   function advertise() {
     bot.chat(
       "Hi, I'm Transbot, for a list of my commands visit tenshi.gay/transbot"
     );
-  }
-
-  function sayUselessFact() {
-    try {
-      https
-        .get("https://uselessfacts.jsph.pl/api/v2/facts/random", (res) => {
-          if (res.statusCode !== 200) {
-            bot.chat(
-              "Sorry, I Failed to fetch the information from the API :("
-            );
-          } else {
-            let data = "";
-
-            // Do cool stuffs
-
-            res.once("data", (chunk) => {
-              data += chunk;
-            });
-
-            res.once("end", () => {
-              const fact = JSON.parse(data).fact;
-              if (fact.length > 254) {
-                bot.chat(
-                  "Sorry gang, the silly fact was too long (>255) to send :("
-                ); // HISS
-              } else {
-                bot.chat(fact); // MEOW
-              }
-            });
-          }
-        })
-        .once("error", (err) => {
-          console.error("Error:", err.message);
-        });
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-  function sayCatFact() {
-    try {
-      https
-        .get("https://catfact.ninja/fact", (res) => {
-          //Getting a little silly wid it >:3
-          if (res.statusCode !== 200) {
-            bot.chat(
-              "Sorry, I Failed to fetch the information from the API :("
-            ); // Cry to chat
-          } else {
-            let data = "";
-
-            // Do cool stuffs
-
-            res.once("data", (chunk) => {
-              data += chunk;
-            });
-
-            res.once("end", () => {
-              const fact = JSON.parse(data).fact;
-              if (fact.length > 254) {
-                bot.chat(
-                  "Sorry gang, the silly fact was too long (>255) to send :("
-                ); // HISS
-              } else {
-                bot.chat(fact); // MEOW
-              }
-            });
-          }
-        })
-        .once("error", (err) => {
-          console.error("Error:", err.message);
-        });
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-  async function goToSleep() {
-    const bed = bot.findBlock({
-      matching: (block) => bot.isABed(block),
-    });
-    if (bed) {
-      try {
-        await bot.sleep(bed);
-        bot.chat("I'm sleeping");
-      } catch (err) {
-        bot.chat(`I can't sleep: ${err.message}`);
-      }
-    } else {
-      bot.chat("No nearby bed");
-    }
-  }
-  async function wakeUp() {
-    try {
-      await bot.wake();
-    } catch (err) {
-      bot.chat(`I can't wake up: ${err.message}`);
-    }
   }
 
   // // Log errors and kick reasons:
