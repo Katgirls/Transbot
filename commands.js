0f5ace0f3e10813e8ad2571ab978a8d4c3f461a5
FelixCharm <105816617+FelixCharm@users.noreply.github.com>
2023-04-20 16:10:41 +0100

    Comply with javascript standard formatting

--- commands.js
+++ commands.js
@@ -1,113 +1,112 @@
-const Discord = require("discord.js");
-const mfpf_pkg = require("mineflayer-pathfinder");
+const Discord = require('discord.js')
+const mfpfPkg = require('mineflayer-pathfinder')
 
-const { EmbedBuilder } = Discord;
-const { Movements, goals } = mfpf_pkg;
-const { GoalNear, GoalFollow } = goals;
+const { EmbedBuilder } = Discord
+const { Movements, goals } = mfpfPkg
+const { GoalNear, GoalFollow } = goals
 
-const config = require("./config.js");
-const features = require("./features.js");
+const config = require('./config.js')
+const features = require('./features.js')
 
-let bot;
+let bot
 
-function set_bot(_bot) {
-  bot = _bot;
+function setBot (_bot) {
+  bot = _bot
 }
 
-function chat(data, channel) {
-  console.log("playerChat");
+function chat (data, channel) {
+  console.log('playerChat')
   try {
     // something is very wrong here :(
     // why is the entire message in senderName!?
-    const senderObj = JSON.parse(data.senderName);
-    const extraArray = senderObj.extra;
-    const senderName = extraArray[0].hoverEvent.contents.text;
-    const message = extraArray[3].extra[0].text;
+    const senderObj = JSON.parse(data.senderName)
+    const extraArray = senderObj.extra
+    const senderName = extraArray[0].hoverEvent.contents.text
+    const message = extraArray[3].extra[0].text
 
-    console.log(senderName + ": " + message);
+    console.log(senderName + ': ' + message)
 
-    const defaultMove = new Movements(bot);
+    const defaultMove = new Movements(bot)
 
-    if (message === "*cat") {
-      features.sayCatFact();
+    if (message === '*cat') {
+      features.sayCatFact()
     }
 
-    if (message === "*fact") {
-      features.sayUselessFact();
+    if (message === '*fact') {
+      features.sayUselessFact()
     }
 
-    if (message === "*stop") {
+    if (message === '*stop') {
       if (!config.whitelist.includes(senderName)) {
-        bot.chat("Sorry, you're not permitted to give me instructions");
+        bot.chat('Sorry, you\'re not permitted to give me instructions')
       } else {
-        bot.chat("Understood, I stopped my current task");
-        bot.pathfinder.stop();
-        config.do_look = true;
-        config.do_farm = false;
+        bot.chat('Understood, I stopped my current task')
+        bot.pathfinder.stop()
+        config.doLook = true
+        config.doFarm = false
       }
     }
 
-    if (message === "*farm") {
+    if (message === '*farm') {
       if (!config.whitelist.includes(senderName)) {
-        bot.chat("Sorry, you're not permitted to give me instructions");
+        bot.chat('Sorry, you\'re not permitted to give me instructions')
       } else {
-        // bot.chat("Feature currently disabled");
-        config.do_look = false;
-        config.do_farm = true;
-        bot.chat("Now farming!");
+        // bot.chat('Feature currently disabled');
+        config.doLook = false
+        config.doFarm = true
+        bot.chat('Now farming!')
       }
     }
 
-    if (message === "*dropall") {
+    if (message === '*dropall') {
       if (!config.whitelist.includes(senderName)) {
-        bot.chat("Sorry, you're not permitted to give me instructions");
+        bot.chat('Sorry, you\'re not permitted to give me instructions')
       } else {
-        features.dropAll();
+        features.dropAll()
       }
     }
 
-    if (message === "*come") {
+    if (message === '*come') {
       if (!config.whitelist.includes(senderName)) {
-        bot.chat("Sorry, you're not permitted to give me instructions");
+        bot.chat('Sorry, you\'re not permitted to give me instructions')
       } else {
-        const target = bot.players[senderName]?.entity;
+        const target = bot.players[senderName]?.entity
         if (!target) {
-          bot.chat("You're not within my visual range");
-          return;
+          bot.chat('You\'re not within my visual range')
+          return
         }
-        bot.chat("Understood, I am now walking to you");
-        const { x: playerX, y: playerY, z: playerZ } = target.position;
+        bot.chat('Understood, I am now walking to you')
+        const { x: playerX, y: playerY, z: playerZ } = target.position
 
-        bot.pathfinder.setMovements(defaultMove);
+        bot.pathfinder.setMovements(defaultMove)
         bot.pathfinder.setGoal(
           new GoalNear(playerX, playerY, playerZ, config.RANGE_GOAL)
-        );
+        )
       }
     }
 
-    if (message === "*follow") {
+    if (message === '*follow') {
       if (!config.whitelist.includes(senderName)) {
-        bot.chat("Sorry, you're not permitted to give me instructions");
+        bot.chat('Sorry, you\'re not permitted to give me instructions')
       } else {
-        const target = bot.players[senderName]?.entity;
+        const target = bot.players[senderName]?.entity
         if (!target) {
-          bot.chat("You're not within my visual range");
-          return;
+          bot.chat('You\'re not within my visual range')
+          return
         }
-        bot.chat("Understood, I am now walking to you");
-        const { x: playerX, y: playerY, z: playerZ } = target.position;
+        bot.chat('Understood, I am now walking to you')
 
-        bot.pathfinder.setMovements(defaultMove);
-        bot.pathfinder.setGoal(new GoalFollow(target, 2), true);
+        bot.pathfinder.setMovements(defaultMove)
+        bot.pathfinder.setGoal(new GoalFollow(target, 2), true)
       }
     }
 
-    if (message === "*sleep") {
-      features.goToSleep();
+    if (message === '*sleep') {
+      features.goToSleep()
     }
 
-    if (message === "*wakeup") {
-      features.wakeUp();
+    if (message === '*wakeup') {
+      features.wakeUp()
     }
 
     const messageEmbed = new EmbedBuilder()
@@ -115,14 +114,14 @@ function chat(data, channel) {
       .setAuthor({
         name: senderName,
         iconURL: `https://mc-heads.net/avatar/${senderName}/`,
-        url: `https://namemc.com/${senderName}`,
+        url: `https://namemc.com/${senderName}`
       })
       .setDescription(message)
-      .setTimestamp();
-    channel.send({ embeds: [messageEmbed] });
+      .setTimestamp()
+    channel.send({ embeds: [messageEmbed] })
   } catch (error) {
-    console.error(error.message);
+    console.error(error.message)
   }
 }
 
-module.exports = { set_bot, chat };
+module.exports = { setBot, chat }
--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 // bot settings
-const ms_email    = "iefn29fing98n3@outlook.com";
-const ms_password = "9Bf$qBk^^yu#NGVn3q2i8a^q3f$hKA";
-const bot_token   = "MTA4NjQxNDM2MzgxNDAwMjg4OQ.GStcCC.xEXdn3iBalqy5x6fuKxJa69LI3CttAdtEqVrx4";
-const channel_id  = "1082122512357404732";
+const msEmail = 'iefn29fing98n3@outlook.com'
+const msPassword = '9Bf$qBk^^yu#NGVn3q2i8a^q3f$hKA'
+const botToken = 'MTA4NjQxNDM2MzgxNDAwMjg4OQ.GStcCC.xEXdn3iBalqy5x6fuKxJa69LI3CttAdtEqVrx4'
+const channelId = '1082122512357404732'
 
 // game settings
-const mc_server   = "play.fennet.rentals";
-const mc_port     = "25565";
-const mc_version  = "1.19.3";
+const mcServer = 'play.fennet.rentals'
+const mcPort = '25565'
+const mcVersion = '1.19.3'
 
 // in-game settings
-const whitelist   = ["Vixy", "cat_yawn", "Egirl39", "AK50"];
-const RANGE_GOAL  = 1;
+const whitelist = ['Vixy', 'cat_yawn', 'Egirl39', 'AK50']
+const RANGE_GOAL = 1
 
 // not constants
-let do_look = true;
-let do_farm = false;
+let doLook = true
+let doFarm = false
 
 module.exports = {
-  ms_email,
-  ms_password,
-  bot_token,
-  channel_id,
-  mc_server,
-  mc_port,
-  mc_version,
+  msEmail,
+  msPassword,
+  botToken,
+  channelId,
+  mcServer,
+  mcPort,
+  mcVersion,
   whitelist,
   RANGE_GOAL,
-  do_look,
-  do_farm,
-};
+  doLook,
+  doFarm
+}
