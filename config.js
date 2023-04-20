c0bc44c99a5e459b3726fbb9c7878482d6dc18b3
kate <kate@fennet.rentals>
2023-04-20 09:18:01 -0400

    make MC server port configurable
    Signed-off-by: kate <kate@fennet.rentals>

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 // bot settings
-const ms_email = "iefn29fing98n3@outlook.com";
+const ms_email    = "iefn29fing98n3@outlook.com";
 const ms_password = "9Bf$qBk^^yu#NGVn3q2i8a^q3f$hKA";
-const bot_token =
-  "MTA4NjQxNDM2MzgxNDAwMjg4OQ.GStcCC.xEXdn3iBalqy5x6fuKxJa69LI3CttAdtEqVrx4";
-const channel_id = `1082122512357404732`;
+const bot_token   = "MTA4NjQxNDM2MzgxNDAwMjg4OQ.GStcCC.xEXdn3iBalqy5x6fuKxJa69LI3CttAdtEqVrx4";
+const channel_id  = "1082122512357404732";
 
 // game settings
-const mc_server = "play.fennet.rentals";
-const mc_version = "1.19.3";
+const mc_server   = "play.fennet.rentals";
+const mc_port     = "25565";
+const mc_version  = "1.19.3";
 
 // in-game settings
-const whitelist = ["Vixy", "cat_yawn", "Egirl39", "AK50"];
-const RANGE_GOAL = 1;
+const whitelist   = ["Vixy", "cat_yawn", "Egirl39", "AK50"];
+const RANGE_GOAL  = 1;
 
 // not constants
 let do_look = true;
@@ -23,6 +23,7 @@ module.exports = {
   bot_token,
   channel_id,
   mc_server,
+  mc_port,
   mc_version,
   whitelist,
   RANGE_GOAL,
