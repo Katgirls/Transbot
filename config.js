95971e5f9e3d9676311c5ba71b73c7efc16539fa
CatgirlApril <105816617+CatgirlApril@users.noreply.github.com>
2024-02-09 12:20:16 -0800

    Remove sensitive information

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /* eslint-disable prefer-const */
 
 // bot settings
-const msEmail = '***REMOVED***'
-const msPassword = '***REMOVED***'
-const botToken = '***REMOVED***'
-const channelId = '***REMOVED***'
+const msEmail = ''
+const msPassword = ''
+const botToken = ''
+const channelId = ''
 
 // game settings
 const mcServer = 'localhost'
@@ -12,7 +12,7 @@ const mcPort = '25565'
 const mcVersion = '1.19.3'
 
 // in-game settings
-const whitelist = ['***REMOVED***', 'cat_yawn', 'Egirl39', '***REMOVED***']
+const whitelist = ['cat_yawn', 'Egirl39']
 const dropBlacklist = ['bread']
 const hoeItems = ['netherite_hoe', 'diamond_hoe', 'golden_hoe', 'iron_hoe', 'stone_hoe', 'wooden_hoe']
 const RANGE_GOAL = 1
